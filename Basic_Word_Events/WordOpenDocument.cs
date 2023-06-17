ee6bba2fe5882461d1e39ef69b7b6ba8024e3094
MariGhayori <118885716+MariGhayori@users.noreply.github.com>
2023-06-17 10:54:14 +0330

    Add files via upload
    Use this code to detect when a document is opened:
         - You can receive data about the opened document in this Event 
         - You can make some changes to the document

---
+++ Basic_Word_Events/WordOpenDocument.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Word = Microsoft.Office.Interop.Word;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Word;
+using System.Diagnostics;
+
+namespace WordOpenDocument
+{
+    public partial class ThisAddIn
+    {
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            Globals.ThisAddIn.Application.DocumentOpen += new Microsoft.Office.Interop.Word.ApplicationEvents4_DocumentOpenEventHandler(this.Application_DocumentOpen);
+        }
+
+        public void Application_DocumentOpen(Microsoft.Office.Interop.Word.Document Doc)
+        {
+            Debug.Print("Document Opened");
+
+            IntPtr hWnd = Process.GetCurrentProcess().MainWindowHandle;
+
+            Debug.Print("Window Handle: " + hWnd);
+            Debug.Print("Document File Path: " + Doc.FullName);
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+        }
+
+        #region VSTO generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}
